b5c3b189e6465c0dd0da16624c2207db556d9714
VihagaYohan <vihagayohan94@gmail.com>
2021-07-24 22:45:30 +0530

    Added Api versioning

--- LandonAPI/Startup.cs
+++ LandonAPI/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -33,6 +34,14 @@ namespace LandonAPI
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "LandonAPI", Version = "v1" });
 			});
 			services.AddRouting(options => options.LowercaseUrls = true);
+			services.AddApiVersioning(options =>
+			{
+				options.DefaultApiVersion = new ApiVersion(1, 0);
+				options.ApiVersionReader = new MediaTypeApiVersionReader();
+				options.AssumeDefaultVersionWhenUnspecified = true;
+				options.ReportApiVersions = true;
+				options.ApiVersionSelector = new CurrentImplementationApiVersionSelector(options);
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
